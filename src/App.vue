257c7aad3c57cc87b089556b807dcfcc67749191
sswq2020 <a309108243@qq.com>
2022-01-30 16:00:59 +0800

    feat: 🎸 can change tab
    1.注意computed的get,set
    2.注意tab布局是flex布局，方向conlumn这点非常重要，涉及到cube-slide-item布局计算

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
   <div id="app">
     <v-header :seller="seller"></v-header>
+    <div class="tab-wrapper">
+        <tab></tab>
+    </div>
   </div>
 </template>
 
 <script>
-import VHeader from 'components/v-header/v-header'
+import VHeader from './components/v-header/v-header'
+import Tab from './components/tab/tab'
 import { getSeller } from 'api/index'
 
 export default {
@@ -26,9 +30,18 @@ export default {
     }
   },
   components: {
-    VHeader
+    VHeader,
+    Tab
   }
 }
 </script>
 
-<style lang="stylus"></style>
+<style lang="stylus" rel="stylesheet/stylus">
+  #app
+   .tab-wrapper
+    position:fixed
+    top:136px
+    left:0
+    right: 0
+    bottom: 0
+</style>
---
+++ src/components/tab/tab.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="tab">
+    <cube-tab-bar
+      v-model="selectedLabel"
+      :showSlider=true
+      :data="tabs"
+      ref="tabBar"
+    >
+    </cube-tab-bar>
+    <div class="slider-wapper">
+      <cube-slide
+        :loop=false
+        :autoPlay=false
+        :show-dots=false
+        :initial-index="index"
+        @change="onChange"
+        ref="slide">
+        <cube-slide-item> 我是Good </cube-slide-item>
+        <cube-slide-item> 我是Rating </cube-slide-item>
+        <cube-slide-item> 我是Seller </cube-slide-item>
+      </cube-slide>
+    </div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  name: 'tab',
+  data() {
+    return {
+      index: 0,
+      tabs: [
+        {
+          label: '商品'
+        },
+                {
+          label: '评价'
+        },
+                {
+          label: '商家'
+        }
+      ]
+    }
+  },
+  computed: {
+    selectedLabel: {
+      get() {
+        return this.tabs[this.index].label
+      },
+      set(newValue) {
+         this.index = this.tabs.findIndex((item, index) => {
+          return item.label === newValue
+         })
+      }
+    }
+  },
+  methods: {
+    onChange(index) {
+        console.log(index)
+        this.index = index
+    }
+  }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+@import '~common/stylus/variable'
+
+.tab
+  display: flex
+  flex-direction:column
+  height: 100%
+  >>>.cube-tab
+       padding: 10px 0
+     .slider-wapper
+       flex:1
+       overflow: hidden
+</style>
