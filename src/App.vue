761b5e7e5168e3a64d6179d3e8aae67e17970bcb
sswq2020 <a309108243@qq.com>
2022-02-05 16:30:14 +0800

    test: 💍 add intialIndex

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <v-header :seller="seller"></v-header>
     <div class="tab-wrapper">
-        <tab :tabs="tabs"></tab>
+        <tab :tabs="tabs" :initialIndex=1></tab>
     </div>
   </div>
 </template>
