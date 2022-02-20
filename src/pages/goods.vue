d75bb14c7c812264872250c9557eaf8990852fe8
sswq2020 <a309108243@qq.com>
2022-02-20 16:49:55 +0800

    feat: 🎸 实现作用域插槽的,对着cube-ui的组件源码直接看比看文档还好

--- src/pages/goods.vue
+++ src/pages/goods.vue
@@ -1,12 +1,33 @@
 <template>
   <div class="goods">
-     <div class="scroll-nav-wrapper">
+    <div class="scroll-nav-wrapper">
       <cube-scroll-nav
-      :side=true
-      :data="goods"
-      :options="scrollOptions"
-      v-if="goods.length"
+        :side="true"
+        :data="goods"
+        :options="scrollOptions"
+        v-if="goods.length"
       >
+        <!-- cube-scroll-nav的作用域插槽旧版,建议看源码文档 -->
+        <template slot="bar" slot-scope="slotProps">
+          <cube-scroll-nav-bar
+            :direction="'vertical'"
+            :txts="barTxt"
+            :labels="slotProps.labels"
+            :current="slotProps.current"
+          >
+                <!-- cube-scroll-nav-bar的作用域插槽旧版,建议看源码文档 -->
+                <template slot-scope="slotsunProps">
+                  <div class="text">
+                    <support-ico
+                      v-if="slotsunProps.txt.type>=1"
+                      :size=3
+                      :type="slotsunProps.txt.type">
+                    </support-ico>
+                    <span>{{slotsunProps.txt.name}}</span>
+                  </div>
+                </template>
+          </cube-scroll-nav-bar>
+        </template>
         <cube-scroll-nav-panel
           v-for="good in goods"
           :key="good.name"
@@ -14,37 +35,35 @@
           :title="good.name"
         >
           <ul>
-            <li
-            v-for="food in good.foods"
-            :key="food.name"
-            class="food-item"
-            >
+            <li v-for="food in good.foods" :key="food.name" class="food-item">
               <div class="icon">
-                <img width="57" height="57"  :src="food.icon">
+                <img width="57" height="57" :src="food.icon" />
               </div>
               <div class="content">
-              <h2 class="name">{{food.name}}</h2>
-              <p class="desc">{{food.description}}</p>
-              <div class="extra">
-                <span class="count">月售{{food.sellCount}}份</span>
-                <span>好评率{{food.rating}}%</span>
+                <h2 class="name">{{ food.name }}</h2>
+                <p class="desc">{{ food.description }}</p>
+                <div class="extra">
+                  <span class="count">月售{{ food.sellCount }}份</span
+                  ><span>好评率{{ food.rating }}%</span>
+                </div>
+                <div class="price">
+                  <span class="now">￥{{ food.price }}</span
+                  ><span class="old" v-show="food.oldPrice"
+                    >￥{{ food.oldPrice }}</span
+                  >
+                </div>
               </div>
-              <div class="price">
-                <span class="now">￥{{food.price}}</span>
-                <span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
-              </div>
-              </div>
-
             </li>
           </ul>
         </cube-scroll-nav-panel>
       </cube-scroll-nav>
-     </div>
+    </div>
   </div>
 </template>
 
 <script>
 import { getGoods } from '../api/index'
+import SupportIco from '../components/support-ico/support-ico.vue'
 
 export default {
   name: 'Goods',
@@ -70,6 +89,21 @@ export default {
   mounted() {
 
   },
+  computed: {
+   barTxt() {
+     let ret = []
+     this.goods.forEach(good => {
+        let count = 0
+        const { type, name, foods } = good
+        foods.forEach((food) => {
+          count = count + food.count || 0
+        })
+        ret.push({ type, name, count })
+     })
+     return ret
+   }
+
+  },
 
   methods: {
     fetch() {
@@ -77,22 +111,122 @@ export default {
         this.goods = goods
       })
     }
+  },
+  components: {
+    SupportIco
   }
 }
 </script>
 
 <style lang="stylus" scoped>
-@import "~common/stylus/mixin";
-@import "~common/stylus/variable";
-.goods
-  position: relative
-  text-align: left
-  height: 100%
-  .scroll-nav-wrapper
-    position:absolute
-    width: 100%
-    top:0
-    left:0
-    bottom: 48px
+@import '~common/stylus/mixin';
+@import '~common/stylus/variable';
 
+.goods {
+  position: relative;
+  text-align: left;
+  height: 100%;
+
+  .scroll-nav-wrapper {
+    position: absolute;
+    width: 100%;
+    top: 0;
+    left: 0;
+    bottom: 48px;
+  }
+
+  >>> .cube-scroll-nav-bar {
+    width: 80px;
+    white-space: normal;
+    overflow: hidden;
+  }
+
+  >>> .cube-scroll-nav-bar-item {
+    padding: 0 10px;
+    display: flex;
+    align-items: center;
+    height: 56px;
+    line-height: 14px;
+    font-size: 12px;
+    background: $color-background-ssss;
+  }
+
+  >>> .cube-scroll-nav-bar-item_active {
+    background: #fff;
+    color: grey;
+  }
+
+  >>> .cube-scroll-nav-panel-title {
+    padding-left: 14px;
+    height: 26px;
+    line-height: 26px;
+    border-left: 2px solid $color-col-line;
+    font-size: 12px;
+    color: grey;
+    background: $color-background-ssss;
+  }
+
+  .food-item {
+    display: flex;
+    margin: 18px;
+    padding-bottom: 18px;
+    position: relative;
+
+    &:last-child {
+      border-none();
+      margin-bottom: 0;
+    }
+
+    .icon {
+      flex: 0 0 57px;
+      margin: rigt 10px;
+    }
+
+    .content {
+      flex: 1;
+
+      .name {
+        margin: 2px 0 8px 0;
+        height: 14px;
+        line-height: 14px;
+        font-size: $fontsize-medium;
+        color: $color-dark-grey;
+      }
+
+      .desc, .extra {
+        line-height: 10px;
+        font-size: $fontsize-small-s;
+        color: $color-light-grey;
+      }
+
+      .desc {
+        line-height: 12px;
+        margin-bottom: 8px;
+      }
+
+      .extra {
+        .count {
+          margin-right: 12px;
+        }
+      }
+
+      .price {
+        font-weight: 700;
+        line-height: 24px;
+
+        .now {
+          margin-right: 8px;
+          font-size: $fontsize-medium;
+          color: $color-red;
+        }
+
+        .old {
+          text-decoration: line-through;
+          font-size: $fontsize-small-s;
+          color: $color-light-grey;
+        }
+      }
+    }
+  }
+}
 </style>
